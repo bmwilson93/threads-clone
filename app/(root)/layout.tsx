b81759059a44aaf3a1d8e13bda2939eed4f488da
Brian Wilson <bmwilson93@gmail.com>
2024-03-14 22:39:10 -0600

    added project assets

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
+import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import "../globals.css";
 import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import RightSidebar from "@/components/shared/RightSidebar";
